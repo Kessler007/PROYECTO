ec94e16309a9c9ece0ac24ee05ca187807d5b672
Kessler007 <Dan.wick2106@gmail.com>
2022-10-19 09:04:47 -0400

    Create Account

--- app/src/main/java/com/example/login_activity/CreateAccountActivity.java
+++ app/src/main/java/com/example/login_activity/CreateAccountActivity.java
@@ -3,16 +3,12 @@ package com.example.login_activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class CreateAccountActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-    public void goCreateAccount(View view){
-        System.out.println("----> go to create account");
+        setContentView(R.layout.activity_create_account);
     }
 }
