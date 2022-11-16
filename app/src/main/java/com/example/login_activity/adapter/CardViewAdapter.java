4726e6ee8fb9e7f30430e14d3efb000c361e6159
Kessler007 <dan.wick2106gmail.com>
2022-11-16 16:46:17 -0400

    ReciclerView

--- app/src/main/java/com/example/login_activity/adapter/CardViewAdapter.java
+++ app/src/main/java/com/example/login_activity/adapter/CardViewAdapter.java
@@ -1,4 +1,68 @@
 package com.example.login_activity.adapter;
 
-public class CardViewAdapter {
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.login_activity.R;
+import com.example.login_activity.model.Image;
+
+import java.util.ArrayList;
+
+public class CardViewAdapter extends RecyclerView.Adapter<CardViewAdapter.CardViewHolder> {
+    //Lista de imagenes
+    private ArrayList<Image> images;
+    private int resource;
+    private Activity activity;
+
+    public CardViewAdapter(ArrayList<Image> images, int resource, Activity activity){
+        this.images = images;
+        this.resource = resource;
+        this.activity = activity;
+    }
+
+    @NonNull
+    @Override
+    public CardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(resource, parent, false);
+
+        return new CardViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CardViewHolder holder, int position) {
+        Image image = images.get(position);
+        //image
+        holder.usernameCardview.setText(image.getUsername());
+        holder.cantidadDiasCardView.setText(image.getCantidadDias());
+        holder.cantidadMegustaCardView.setText(image.getCantidadMeusta());
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.size();
+    }
+
+    public class CardViewHolder extends RecyclerView.ViewHolder{
+        private ImageView imageCardView;
+        private TextView usernameCardview;
+        private TextView cantidadDiasCardView;
+        private TextView cantidadMegustaCardView;
+
+        public CardViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageCardView = itemView.findViewById(R.id.imageCardView);
+            usernameCardview = itemView.findViewById(R.id.usernameCardView);
+            cantidadDiasCardView = itemView.findViewById(R.id.cantidadDiasCardView);
+            cantidadMegustaCardView = itemView.findViewById(R.id.cantidadMeGustaCardView);
+
+        }
+    }
 }
